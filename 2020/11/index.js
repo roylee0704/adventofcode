01064a05d0de18a12b7632bf4136ca8f8883b9e1
roylee0704 <roylee0704@gmail.com>
2020-12-11 20:21:23 +0700

    day11

--- 2020/11/index.js
+++ 2020/11/index.js
@@ -1,116 +1,47 @@
 import fs from 'fs';
 
-function part1(seats) {
-    const newSeats = seats.map(seat => seat.slice());
-    for (let row = 0; row < seats.length; row++) {
-        for (let col = 0; col < seats[0].length; col++) {
-            if (seats[row][col] === 'L' && countAdjacents(seats, row, col, isOccupied) === 0) {
-                newSeats[row][col] = '#';
+function solve(grid) {
+    const newGrid = [];
+    for (let row = 0; row < grid.length; row++) {
+        let newRow = '';
+        for (let col = 0; col < grid[0].length; col++) {
+            const adj = [];
+            for (const dx of [0, -1, 1]) {
+                for (const dy of [0, -1, 1]) {
+                    if (dx === 0 && dy === 0) {
+                        continue;
+                    }
+                    // work within the boundary
+                    if (row + dx >= 0 && row + dx < grid.length && col + dy >= 0 && col + dy < grid[0].length && grid[row + dx][col + dy] === '#') {
+                        adj.push('#');
+                    }
+                }
             }
-            if (seats[row][col] === '#' && countAdjacents(seats, row, col, isOccupied) >= 4) {
-                newSeats[row][col] = 'L';
+
+            if (grid[row][col] === 'L' && !adj.includes('#')) {
+                newRow += '#';
             }
+            else if (grid[row][col] === '#' && adj.length >= 4) {
+                newRow += 'L';
+            } else {
+                newRow += grid[row][col];
+            }
+
         }
+        newGrid.push(newRow);
     }
-    return newSeats;
+    return newGrid;
 }
 
-function part2(seats) {
-    const newSeats = seats.map(seat => seat.slice());
-    for (let row = 0; row < seats.length; row++) {
-        for (let col = 0; col < seats[0].length; col++) {
-            if (seats[row][col] === 'L' && countAdjacents(seats, row, col, isOccupiedGreedy) === 0) {
-                newSeats[row][col] = '#';
-            }
+let grid = fs.readFileSync('./input.txt', 'utf-8').split('\n');
+while (true) {
+    const nextGrid = solve(grid);
 
-            if (seats[row][col] === '#' && countAdjacents(seats, row, col, isOccupiedGreedy) >= 5) {
-                newSeats[row][col] = 'L';
-            }
-        }
+    console.log(nextGrid.join(''), 'hehe');
+    if (nextGrid.join('') === grid.join('')) {
+        console.log(nextGrid.join('').split('').filter(rc => rc === '#').length)
+        break;
     }
-    return newSeats;
+
+    grid = nextGrid;
 }
-
-
-function countAdjacents(seats, row, col, formula) {
-    let count = 0;
-    [-1, 0, 1].forEach(up =>
-        [-1, 0, 1].forEach(down => {
-            if (up !== 0 || down !== 0) {
-                count += formula(seats, row, col, up, down);
-            }
-        })
-    )
-    return count;
-}
-
-
-function isOccupied(seats, row, col, up, down) {
-    let dRow = row + up;
-    let dCol = col + down;
-
-    if (dRow < 0 || dCol < 0 || dRow >= seats.length || dCol >= seats[0].length) {
-        return 0;
-    }
-    return seats[dRow][dCol] === '#' ? 1 : 0 ?? 0;
-}
-
-function isOccupiedGreedy(seats, row, col, up, down) {
-    let dRow = row + up;
-    let dCol = col + down;
-
-    if (dRow < 0 || dCol < 0 || dRow >= seats.length || dCol >= seats[0].length) {
-        return 0;
-    }
-    if (seats[dRow][dCol] === '#') {
-        return 1;
-    }
-    if (seats[dRow][dCol] === 'L') {
-        return 0;
-    }
-    return isOccupiedGreedy(seats, dRow, dCol, up, down);
-}
-
-function isEqual(m1, m2) {
-    for (let i = 0; i < m1.length; i++) {
-        for (let j = 0; j < m1[0].length; j++) {
-            if (m1[i][j] !== m2[i][j]) {
-                return false
-            }
-        }
-    }
-    return true;
-}
-
-
-function countOccupied(seats) {
-    let sum = 0;
-    for (let i = 0; i < seats.length; i++) {
-        for (let j = 0; j < seats[0].length; j++) {
-            if (seats[i][j] === '#') {
-                sum++;
-            }
-        }
-    }
-    return sum;
-}
-
-function simulate(prog, initialState) {
-    let currState = initialState;
-    while (true) {
-        const nextState = prog(currState);
-        if (isEqual(currState, nextState)) {
-            break;
-        }
-        currState = nextState;
-    }
-    return currState;
-}
-
-
-const lines = fs.readFileSync('./input.txt', 'utf-8').split('\n');
-const initialState = lines.map(line => [...line.split('')]);
-
-console.log('part1:', countOccupied(simulate(part1, initialState)))
-console.log('part2:', countOccupied(simulate(part2, initialState)))
-

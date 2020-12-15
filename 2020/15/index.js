b66432017bd2c3fbb76c750ec14064aaf2491130
roylee0704 <roylee0704@gmail.com>
2020-12-15 13:27:25 +0700

    day15

---
+++ 2020/15/index.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+function solve(lines) {
+    const spoken = {};
+    let last = 0;
+    let turn = 1;
+    for (let i = 0; i < lines.length; i++) {
+        spoken[lines[i]] = [i + 1];
+        last = lines[i];
+        turn++;
+    }
+
+    while (turn < 30000000) {
+        last = spoken[last]?.length >= 2 ?
+            spoken[last][spoken[last].length - 1] - spoken[last][spoken[last].length - 2] :
+            0;
+        if (!spoken[last]) {
+            spoken[last] = [];
+        }
+
+        spoken[last].push(turn);
+
+        if (turn % 1000000 === 0) {
+            console.log(`speak (${turn}):`, last)
+        }
+        turn++;
+    }
+    console.log('last spoken:', last)
+}
+
+let lines = fs.readFileSync('./input.txt', 'utf-8').split(',');
+solve(lines);
